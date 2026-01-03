0df027617b3041df913b44045cef853594a07e1c
karolina utsi <id23kui@cs.umu.se>
2026-01-03 21:07:49 +0100

    skapat metoden IsItAMatch i RoundMethod. tror den är rätt???

--- Models/RoundMethod.cs
+++ Models/RoundMethod.cs
@@ -1,7 +1,22 @@
 ﻿namespace MEMORY.Models
 {
+    //ska vi ha en konstruktor som hämtar korten från databasen?????
     public class RoundMethod
     {
-
+        //metod för att kolla om det är en match
+        //tar in två kortnamn och returnerar true eller false
+        //ska metoden bara ta in kortnamnen eller hela RoundDetail objekt?????
+        public Boolean IsItAMatch(string cardName1, string cardName2)
+        {
+            if (cardName1 == cardName2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
+    //Ska vi skapa konstruktor här?????
 }
